49635054dee88d4ef6b0afedbd5932a3d480c883
italorochaj <italorochaj@icloud.com>
2025-01-21 15:29:10 -0300

    components dinamycs properties

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent implements OnInit {
+
+  @Input()
+  photoCover:string="https://media.investnews.com.br/uploads/2023/07/400395744-1.jpg"
+  @Input()
+  cardTitle:string="Openai"
 
   constructor() { }
 
